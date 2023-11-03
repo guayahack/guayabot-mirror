3d2ff1b4268ab9f376833c8db2641629f9583281
mcveigth <mcveigth@protonmail.com>
2023-11-02 23:23:33 -0600

    deprecates split_string_with_quotes with filter using regex, fixes special characters saved in quotes.yaml, and clean some parts of the code

--- quotes.py
+++ quotes.py
@@ -1,6 +1,6 @@
 from yaml import safe_load, dump
 
-def read_yaml_quotes(yaml_file:str):
+def read_yaml_quotes(yaml_file:str) -> list:
     """
     Reads a YAML file containing quotes and authors, and returns a list of quote-author pairs.
 
@@ -8,7 +8,7 @@ def read_yaml_quotes(yaml_file:str):
         yaml_file (str): The path to the YAML file containing quotes and authors.
 
     Returns:
-        list: A list of lists, where each inner list contains an author's name and their associated quote.
+        quote_list (list): A list of lists, where each inner list contains an author's name and their associated quote.
 
     This function reads a YAML file and extracts quote-author pairs from the 'quotes' section
     of the YAML content. It returns the data in the form of a list, where each inner list
@@ -26,7 +26,7 @@ def read_yaml_quotes(yaml_file:str):
     
     return quote_list
 
-def add_quote(quote_list:list, yaml_file:str):
+def add_quote(quote_list:list, yaml_file:str) -> None:
     """
     Appends a new quote and author to an existing YAML file in a specific format.
 
@@ -46,16 +46,13 @@ def add_quote(quote_list:list, yaml_file:str):
 
     quotes = data.get("quotes", {})
 
-    if quote.startswith('"') and quote.endswith('"'):
-        quote = quote[1:-1]
-
     quotes[author] = quote
     data["quotes"] = quotes
 
     with open(yaml_file, 'w') as file:
-        dump(data, file, default_flow_style=False)
+        dump(data, file, default_flow_style=False, allow_unicode=True)
 
-def format_quote(quote_list:list):
+def format_quote(quote_list:list) -> str:
     """
     Formats a quote and its author's name with an aesthetically pleasing font style.
 
@@ -64,7 +61,7 @@ def format_quote(quote_list:list):
             associated value is the quote text.
 
     Returns:
-        str: A formatted representation of the quote with the author, suitable for display.
+        formatted_quote (str): A formatted representation of the quote with the author, suitable for display.
 
     This function takes a dictionary containing the author's name as the key and their
     associated quote text as the value. It combines them into a well-designed
--- responses.py
+++ responses.py
@@ -1,39 +1,53 @@
 import random
 import quotes
+import re
 
-def split_string_with_quotes(input_string):
-    parts = []  # Initialize an empty list to store the segments
-    inside_quotes = False  # Track if we are currently inside double quotes
-    current_part = ''  # Initialize the current part as an empty string
+def filter(message:str) -> list:
+    """
+    Filters a message to extract a command, author, and a quote.
 
-    for char in input_string:
-        if char == ' ' and not inside_quotes:
-            if current_part:  # If the current part is not empty, add it to the list
-                parts.append(current_part)
-            current_part = ''  # Reset the current part
-        else:
-            current_part += char
-            if char == '"':
-                inside_quotes = not inside_quotes  # Toggle the inside_quotes flag
+    Args:
+        message (str): The input message to be filtered.
 
-    if current_part:
-        parts.append(current_part)  # Append the last part after the loop
+    Returns:
+        parts (list): A list containing the extracted information.
+              - If the message matches the expected pattern:
+                [command (str), author (str), quote (str)]
+              - If the message does not match the pattern:
+                [message (str), " " (str)]
+    
+    The function uses a regular expression pattern to parse the input message and extracts
+    a command, an optional author, and a quoted text. If the pattern is not matched,
+    it returns the original message with an empty string as the author.
 
-    return parts
+    Example:
+    >>> filter('!addquote John "This is a quote."')
+    ['addquote', 'John', 'This is a quote.']
+
+    >>> filter('Hello, world!')
+    ['Hello, world!', ' ']
+    """    
+    # Define the regex pattern
+    pattern = r'!(addquote|showquote)(?: (\w+))? "(.*?)"'
+
+    match = re.match(pattern, message)
+    if match:
+        command, author, quote = match.groups()
+        return command, author, quote
+    else:
+        return message, " "
 
 def handle_response(message) -> str:
     p_message = message.lower()
 
-    parts = split_string_with_quotes(p_message)
+    parts = filter(p_message)
     command = parts[0]
 
     if command == '!quote':
         quotes_list = quotes.read_yaml_quotes("quotes.yaml")
         return quotes.format_quote(random.choice(quotes_list))
 
-    if command == '!addquote':
-        #for now please provide author quote only
-        #command should be !addquote author "quote"
+    if command == 'addquote':
         quote_list = []
         quote_list.extend([parts[1], parts[2]])
         quotes.add_quote(quote_list, "quotes.yaml")
